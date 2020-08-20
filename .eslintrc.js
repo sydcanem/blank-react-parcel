e52b6fa1eaeca2e192f6c7bea057829de1185368
James Santos <icqhv.santos@gmail.com>
2020-08-20 21:58:53 +1200

    initial scaffolding: react,parcel,eslint,prettier

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react-hooks"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+    "prettier/prettier": ["error", { "singleQuote": true }],
+    quotes: [2, "single", "avoid-escape"]
+  }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Blank JavaScript React project
+
+A bare-bones React starter project using [Parcel](https://parceljs.org/).
